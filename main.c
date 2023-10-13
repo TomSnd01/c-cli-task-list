5371a2763aa782de3a620e32a1266cf5a99e22dc
Tom <tom.sander243@gmail.com>
2023-10-13 21:28:24 +0200

    idfk

--- main.c
+++ main.c
@@ -3,12 +3,18 @@
 #include <argtable3.h>
 
 struct arg_lit *help, *version;
+struct arg_file *dir;
 struct arg_end *end;
 
-int main(int argc, char *argv) {
+int main(int argc, char **argv) {
     void *argtable[] = {
         help = arg_litn("h", "help", 0, 1, "Display this help and exit"),
         version = arg_litn("V", "version", 0, 1, "Display version info and exit"),
+        end = arg_end(20),
+    };
+
+    void *dir_argtable[] = {
+        dir = arg_filen(NULL, NULL, "<dir>", 1, 1, "Directory to storage"),
         end = arg_end(20),
     };
 
@@ -19,29 +25,45 @@ int main(int argc, char *argv) {
     if (nerrors > 0) {
         arg_print_errors(stdout, end, "c-task");
         arg_print_glossary(stdout, argtable, " %-25s %s\n");
-        arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
         exitcode = 1;
         goto exit;
     }
 
-    if (help->count > 0) {
-        printf("Usage: c-task");
-        arg_print_syntax(stdout, argtable, "\n");
-        printf("This program does something.\n");
-        arg_print_glossary(stdout, argtable, " %-25s %s\n");
-        arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+    if(strcmp(argv[1], "dir") == 0) {
+        nerrors = arg_parse(argc, argv, argtable);
+        if (nerrors > 0) {
+            arg_print_errors(stdout, end, "c-task");
+            arg_print_glossary(stdout, argtable, " %-25s %s\n");
+            exitcode = 1;
+            goto exit;
+        }
+        printf("dir: %s\n", dir->filename[0]);
         exitcode = 0;
         goto exit;
+    } 
+    else {
+        if (help->count > 0) {
+            printf("Usage: c-task");
+            arg_print_syntax(stdout, argtable, "\n");
+            arg_print_syntax(stdout, dir_argtable, "\n");
+            printf("CLI Task List / Tracker.\n");
+            arg_print_glossary(stdout, argtable, " %-25s %s\n");
+            arg_print_glossary(stdout, dir_argtable, " %-25s %s\n");
+            exitcode = 0;
+            goto exit;
+        }
+        if (version->count > 0) {
+            printf("c-cli-task-list version 1.0\n");
+            exitcode = 0;
+            goto exit;
+        }
     }
 
-    if (version->count > 0) {
-        printf("c-cli-task-list version 1.0\n");
-        arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
-        exitcode = 0;
-        goto exit;
-    }
+    printf("exitcode: %d\n", exitcode);
 
 exit:
     arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+    arg_freetable(dir_argtable, sizeof(dir_argtable) / sizeof(dir_argtable[0]));
+    printf("exitcode: %d\n", exitcode);
     return exitcode;
 }
